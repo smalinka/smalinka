303ff9c1bc2efe24e2e6aa62febdcbb7f7a5b160
Your Name <you@example.com>
2019-08-14 18:07:44 +0200

    pierwszy commit

---
+++ projekt.py
@@ -0,0 +1,27 @@
+napis = "Ajvgfj"
+if napis.isnumeric:
+    print(False)
+else:
+    print(True)
+
+
+
+a = "ala ma kota"
+a = a.replace('ala', 'agnieszka'), ('kota', 'psa')
+print(a)
+
+
+
+a = "10, 9, 8, 7, 3"
+print(tuple(a.split(", ")))
+
+
+plik="C:users:aa:pli.k.txt"
+print(plik.split(".")[-1])
+
+
+
+
+
+a = "1, 2, 3, 4, 5"
+a.replace(", ", "+")
